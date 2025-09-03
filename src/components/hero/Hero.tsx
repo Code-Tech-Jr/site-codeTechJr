cf76c410ec545df1cc3999e7c559fc1edefa31d2
Igor Reis <irgigor04@gmail.com>
2025-09-02 22:37:22 -0300

    task[7] Hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,15 @@
 import global from "../../assets/styles/global.module.css"
-
+import Button from "../button/Button"
 import styles from "./Hero.module.css"
 
 function Hero() {
     return (
         <div className={styles["container-hero"]}>
-            <h1>Hero</h1>
+            <h1 className={styles["hero-title"]}>A tecnologia que{'\n'} fala a <span className={styles["highlight"]}>sua</span> língua</h1>
+            <div className={styles["hero-buttons"]}>
+                <Button nomeDoBotao="NOSSOS SERVIÇOS" href="#container-our-services" variant="hero"/>
+                <Button nomeDoBotao="ENTRE EM CONTATO" href="#container-talk-to-us" variant="hero"/>
+            </div>
         </div>
         
     )
